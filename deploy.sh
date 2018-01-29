7638bc21748ba7c736d27038a5142de1d33480d9
chess126 <chess99@126.com>
2018-01-29 14:33:54 +0800

    update book

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf _book
+gitbook build
+cp assets/circle.yml _book/circle.yml
+cp assets/CNAME _book/CNAME
+cd _book
+git init
+git add -A
+git commit -m 'update book'
+# git push -f git@github.com:vuejs/vuex.git master:gh-pages
+git push -f git@github.com:chess126/chess126.github.io.git master:gh-pages
+
